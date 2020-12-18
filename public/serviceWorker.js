f60bf544254ab8cff749925012e8a8d94bc0ed7b
m1chu82pl <criminal.industries@gmail.com>
2020-12-18 14:54:41 +0100

    add firebase-messaging-sw.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -16,7 +16,6 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
   event.respondWith(
     caches.match(event.request).then(() => {
       return fetch(event.request).catch(() => caches.match("/offline.html"));
--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, {useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase'; 
 
 function App() {
+
+useEffect(() => {
+  const messaging = firebase.messaging();
+  messaging.requestPermission().then(()=>{
+    console.log('have permission');
+    return messaging.getToken();
+  })
+  .then((token) => {
+    console.log(token);
+  })
+  .catch((error) => console.log(error))
+})
+
   return (
     <div className="App">
       <header className="App-header">
