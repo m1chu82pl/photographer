15ffb6afaba1a12e3fdc511095e0565f1fb93a12
m1chu82pl <criminal.industries@gmail.com>
2020-12-21 13:44:57 +0100

    the token is a live!

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+importScripts("https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.1/firebase-analytics.js");
+
+firebase.initializeApp({
+  messagingSenderId: "797799549033",
+});
